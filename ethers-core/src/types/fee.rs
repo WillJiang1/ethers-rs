7c79cff212a803ba5d53a413d6e212da545c847e
WillJiang1 <jianliiin96@gmail.com>
2024-11-07 13:17:56 +0000

    fix gas used ration

--- ethers-core/src/types/fee.rs
+++ ethers-core/src/types/fee.rs
@@ -6,6 +6,7 @@ use serde::{Deserialize, Serialize};
 pub struct FeeHistory {
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub base_fee_per_gas: Vec<U256>,
+    #[serde(deserialize_with = "gas_used_ratio_deser::deserialize")]
     pub gas_used_ratio: Vec<f64>,
     #[serde(deserialize_with = "deserialize_stringified_numeric")]
     /// oldestBlock is returned as an unsigned integer up to geth v1.10.6. From
@@ -17,4 +18,18 @@ pub struct FeeHistory {
     /// zeroes are returned if the block is empty.
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub reward: Vec<Vec<U256>>,
+}
+
+mod gas_used_ratio_deser {
+    use serde::{Deserialize, Deserializer};
+
+    pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<Vec<f64>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        // First try to deserialize as Option<Vec<f64>>
+        let opt = Option::<Vec<f64>>::deserialize(deserializer)?;
+        // Return empty vec if null, otherwise return the vec
+        Ok(opt.unwrap_or_default())
+    }
 }
